e600a93cbc52596f1ff3f2de485762e436de7627
samyak jain <dosomework2@gmail.com>
2025-07-07 18:42:53 +0530

    change

--- Real estate/src/App.jsx
+++ Real estate/src/App.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Index from "./Pages/Index";
 import PropertyDetail from "./Pages/PropertyDetail";
-import NotFound from "./pages/NotFound";
+import NotFound from "./Pages/NotFound.jsx";
 
 const queryClient = new QueryClient();
 
